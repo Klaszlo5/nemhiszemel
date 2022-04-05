cb96581a99564cb16e4dfad6ca966f38a87e7ba7
Klaszlo5 <gplays131@gmail.com>
2022-04-05 16:43:22 +0200

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+window.addEventListener("load",init)
+function $(elem){
+    return document.querySelectorAll(elem);
+}
+function init(){
+    var piros=document.querySelector(".sarga");
+    var lampa=$(".lampa")
+    var sarga=document.querySelector(".sarga");
+    var zold=document.querySelector(".zold");
+    sarga.getElementsByClassName.opacity="20%";
+    zold.getElementsByClassName.opacity="20%";
+    setInterval(() => {
+        sarga.getElementsByClassName.opacity="100%";
+    },1000);
+    setInterval(() => {
+        if (allapot){
+
+        }
+
+    },1000);
+}
